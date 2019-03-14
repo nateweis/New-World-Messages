54c4f31747b64aee70f76d7bf9d1dbdfde15d80a
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-03-13 20:44:42 -0400

    more comment outs

--- server.js
+++ server.js
@@ -45,17 +45,17 @@ app.use(session({
 //              Controllers
 // =======================================
 
-const userController = require('./controllers/userRoutes.js')
-app.use('/users', userController);
-
-const sessionsController = require('./controllers/sessionsRoutes.js')
-app.use('/sessions', sessionsController);
-
-const chatController = require('./controllers/chatRoutes.js')
-app.use('/chats', chatController)
-
-const messageController = require('./controllers/messageRoute.js')
-app.use('/messages', messageController)
+// const userController = require('./controllers/userRoutes.js')
+// app.use('/users', userController);
+//
+// const sessionsController = require('./controllers/sessionsRoutes.js')
+// app.use('/sessions', sessionsController);
+//
+// const chatController = require('./controllers/chatRoutes.js')
+// app.use('/chats', chatController)
+//
+// const messageController = require('./controllers/messageRoute.js')
+// app.use('/messages', messageController)
 
 
 
@@ -64,37 +64,37 @@ app.use('/messages', messageController)
 // =======================================
 
 
-// io.on('connection',(socket) => {
-//   console.log("fired a socket event", socket.id);
-//
-//   let currentRoom = "nothing"
-//
-//   socket.on('leave',(room) => {
-//     socket.leave(room)
-//   })
-//
-//   socket.on('room',(room) => {
-//     currentRoom = room
-//     socket.join(room)
-//   })
-//
-//   socket.on('message',(newMessage) => {
-//     db.none('INSERT INTO messages (chat_id,sender,message,user_id) VALUES (${chat_id},${sender},${msg},${user_id})', newMessage)
-//     .then(() => {
-//       db.one('SELECT * FROM messages ORDER BY id DESC LIMIT 1;')
-//       .then((data) => {
-//         console.log(data);
-//         io.to(currentRoom).emit('chat', data)
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         console.log("new message retrive isnt working");
-//       })
-//     })
-//   })
-//
-//
-// })
+io.on('connection',(socket) => {
+  console.log("fired a socket event", socket.id);
+
+  let currentRoom = "nothing"
+
+  socket.on('leave',(room) => {
+    socket.leave(room)
+  })
+
+  socket.on('room',(room) => {
+    currentRoom = room
+    socket.join(room)
+  })
+
+  socket.on('message',(newMessage) => {
+    db.none('INSERT INTO messages (chat_id,sender,message,user_id) VALUES (${chat_id},${sender},${msg},${user_id})', newMessage)
+    .then(() => {
+      db.one('SELECT * FROM messages ORDER BY id DESC LIMIT 1;')
+      .then((data) => {
+        console.log(data);
+        io.to(currentRoom).emit('chat', data)
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("new message retrive isnt working");
+      })
+    })
+  })
+
+
+})
 
 
 
